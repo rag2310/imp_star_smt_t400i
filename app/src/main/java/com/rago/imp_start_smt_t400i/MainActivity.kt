60c148ba01d060dc394a5f5ffccb3700ba5c1708
Rodolfo Gutierrez <rag.ortega@gmail.com>
2021-02-01 12:14:33 -0600

    first commit

---
+++ app/src/main/java/com/rago/imp_start_smt_t400i/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rago.imp_start_smt_t400i
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
